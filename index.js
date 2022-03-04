acfa0a37790711e2a8d3cd624d6275f617e9b280
MikeManzi <manzimike378@gmail.com>
2022-03-04 20:21:09 +0200

    (chore): added function to change bg color

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const changeBg = () => {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    document.getElementsByClassName("container__content__left").style.background = "#" + randomColor;
+    document.getElementById("left-panel").style.background = "#" + randomColor;
 }
