4399edb0664a3bfaecd6ca8c72ea676d8b662f24
MikeManzi <manzimike378@gmail.com>
2022-03-04 20:43:13 +0200

    (fix): fixed change color function

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const changeBg = () => {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    document.getElementById("left-panel").style.background = "#" + randomColor;
+    const elements = document.getElementsByClassName("dynamic-bg")
+    const bullets = document.getElementsByClassName("dynamic-bullets")
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "#" + randomColor;
+        bullets[i].style.color = "#" + randomColor
+    }
 }
