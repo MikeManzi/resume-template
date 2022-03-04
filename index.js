d388371298f964db0f464d0da325f65de6ab60de
MikeManzi <manzimike378@gmail.com>
2022-03-04 21:14:44 +0200

    (chore): cleanup

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const changeBg = () => {
+const changeColor = () => {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     const elements = document.getElementsByClassName("dynamic-bg")
-    const bullets = document.getElementsByClassName("dynamic-bullets")
+    const bullets = document.getElementsByClassName("dynamic-color")
     for(var i = 0; i < elements.length; i++) {
         elements[i].style.backgroundColor = "#" + randomColor;
+    }
+    for(var i = 0; i < bullets.length; i++) {
         bullets[i].style.color = "#" + randomColor
     }
 }
